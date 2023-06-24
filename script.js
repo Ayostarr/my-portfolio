43ffc215a53743ea4f2deac51cc59431870decc2
Adewale Ayomide <adewaleayomide24@gmail.com>
2023-06-24 16:15:32 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+var tablinks = document.getElementsByClassName('tab-links');
+var tabcontents = document.getElementsByClassName('tab-contents');
+
+function opentab(tabname) {
+    for (tablink of tablinks) {
+        tablink.classList.remove("active-links");
+    }
+
+    for (tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-links");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+
+
+
+
+
+
+
+const msg = document.getElementById('msg')
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+            msg.innerHTML = "Message sent successfully"
+            setTimeout(function () {
+                msg.innerHTML = ""
+            }, 5000)
+            form.reset()
+        })
+        .catch(error => console.error('Error!', error.message))
+
+})
+
+window.onload = function () {
+    var preloader = document.getElementById('preloader');
+    preloader.style.display = "none";
+}
+
